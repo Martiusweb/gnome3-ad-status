55957687331991f1204f16c1a188c652b7aec88e
Martin Richard <martius@martiusweb.net>
2015-02-18 17:15:37 +0100

    Main UI tweaks
    - Extension bootstraped
    - Can change the color of gnome-shell top panel according to the level of the
      notification
    - push a notification to the user
    
    Signed-off-by: Martin Richard <martius@martiusweb.net>

---
+++ ad-status@martiusweb.net/extension.js
@@ -0,0 +1,185 @@
+/**
+ * Author: Martin Richard <martius@martiusweb.net>
+ * This code is free software, licensed under GNU/LGPL (as are gnome
+ * libraries).
+ */
+
+const St = imports.gi.St;
+const Lang = imports.lang;
+const Main = imports.ui.main;
+const MessageTray = imports.ui.messageTray;
+
+const Levels = ["_normal", "success", "notice", "info", "warn", "critical", "error"];
+const Urgency = {
+    "notice": MessageTray.Urgency.NORMAL,
+    "info": MessageTray.Urgency.NORMAL,
+    "success": MessageTray.Urgency.NORMAL,
+    "warn": MessageTray.Urgency.HIGH,
+    "critical": MessageTray.Urgency.CRITICAL,
+    "error": MessageTray.Urgency.CRITICAL,
+}
+
+// Duration of the blink effect in seconds
+const BLINK_DURATION = 2;
+
+// Blink frequency in Hz
+const BLINK_FREQUENCY = 8;
+
+let ad_status_ui;
+
+const adStatusUi = new Lang.Class({
+    Name: "adStatusUi",
+
+    _init: function() {
+        this._current_level = 0;
+        this._blinking = false;
+        this._blinking_handle = 0;
+    },
+
+    enable: function() {
+        this._source = new MessageTray.Source("alwaysdata", "emblem-important");
+        this._addPanelIcon();
+    },
+
+    disable: function() {
+        this.resetLevel();
+        this._removePanelIcon();
+        this._source.destroy();
+        this._source = null;
+    },
+
+    _addPanelIcon: function() {
+        this._button = new St.Bin({style_class: 'panel-button',
+                                   reactive: true,
+                                   can_focus: true,
+                                   x_fill: true,
+                                   y_fill: false,
+                                   track_hover: true });
+
+        // let icon = new St.Icon({icon_name: 'software-update-urgent-symbolic',
+        let icon = new St.Icon({icon_name: 'software-update-available-symbolic',
+                                style_class: 'system-status-icon'});
+
+        this._button.set_child(icon);
+        Main.panel._rightBox.insert_child_at_index(this._button, 0);
+        this._button.connect('button-press-event', Lang.bind(this, this._buttonPressed));
+    },
+
+    _removePanelIcon: function() {
+        Main.panel._rightBox.remove_child(this._button);
+    },
+
+    _buttonPressed: function() {
+        // Testing
+        if(this._current_level == 0) {
+            this.page("warn", "Test warn!");
+        }
+        else {
+            this.page("critical", "Test critical!");
+        }
+    },
+
+    page: function(level, message) {
+        this.setLevel(level);
+
+        if(!Main.messageTray.contains(this._source)) {
+            Main.messageTray.add(this._source);
+        }
+
+        let params, notification;
+        if(level == "critical" || level == "error") {
+            params = {'soundFile': '/usr/share/sounds/KDE-Sys-App-Negative.ogg'};
+        }
+        else if(level == "success") {
+            params = {'soundFile': '/usr/share/sounds/KDE-Sys-App-Positive.ogg'};
+        }
+
+        notification = new MessageTray.Notification(this._source,
+                                                    "alwaysdata status",
+                                                    message, params);
+        notification.setUrgency(Urgency[level]);
+        this._source.notify(notification);
+    },
+
+    setLevel: function(level) {
+        let level_int = Levels.indexOf(level);
+
+        if(level_int > 0 && level_int >= this._current_level) {
+            this._current_level = level_int;
+            this._makePanelBlink();
+        }
+    },
+
+    resetLevel: function() {
+        this._current_level = 0;
+
+        if(this._blinking) {
+            this._cancelBlinking();
+        }
+
+        Levels.forEach(function(level) {
+            Main.panel._removeStyleClassName('ad-' + level);
+        });
+    },
+
+    _makePanelBlink: function() {
+        if(this._blinking) {
+            this._cancelBlinking();
+        }
+
+        let css_class = 'ad-' + Levels[this._current_level],
+            period = 1000 / BLINK_FREQUENCY,
+            remaining_time = BLINK_DURATION * 1000,
+            blink_on, blink_off;
+
+        blink_on = Lang.bind(this, function() {
+            Main.panel._addStyleClassName(css_class);
+            remaining_time -= period * 2;
+            if(remaining_time > 0) {
+                this._blinking_handle = Main.Mainloop.timeout_add(period, blink_off);
+            }
+            else {
+                this._blinking_handle = 0;
+                this._blinking = false;
+            }
+        });
+
+        blink_off = Lang.bind(this, function() {
+            Main.panel._removeStyleClassName(css_class);
+            this._blinking_handle = Main.Mainloop.timeout_add(period, blink_on);
+        });
+
+        this._blinking = true;
+        blink_on();
+    },
+
+    _cancelBlinking: function() {
+        if(!this._blinking) {
+            return;
+        }
+
+        Main.Mainloop.source_remove(this._blinking_handle);
+        this._blinking_handle = 0;
+        this._blinking = false;
+    },
+});
+
+const adXMPPDaemon = new Lang.Class({
+    Name: "adXMPPDaemon",
+
+    _init: function() {
+        // Ask systemd to start a background task?
+    },
+});
+
+function init() {
+    ad_status_ui = new adStatusUi();
+}
+
+function enable() {
+    ad_status_ui.enable();
+}
+
+function disable() {
+    ad_status_ui.disable();
+}
